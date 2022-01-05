336e403748bba550e87783af7897820c12b9b1c4
c0ndi <kowalik0konrad@gmail.com>
2022-01-05 12:56:48 +0100

    Font fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,25 @@
 module.exports = {
-    content: [
+	content: [
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}',
-    ],
-    theme: {
-        screens: {
-            sm: '480px',
-            md: '768px',
-            lg: '976px',
-            xl: '1440px',
-        },
-        fontWeight: {
-            extraLight: 100,
-            thin: 200,
-            light: 300,
-            normal: 400,
-            medium: 500,
-            semibold: 600,
-            bold: 700,
-            extrabold: 800,
-            black: 900,
-        },
-        extend: {
-            colors: {
-                darkGray: '#0F0F0F',
-                lightGray: '#C1C1C1',
-            },
-            fontFamily: {
-                sans: ['Montserrat', 'sans-serif'],
-            },
-        },
-    },
-    plugins: [],
+	],
+	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		fontFamily: {
+			montserrat: ['Montserrat', 'Arial'],
+			roboto: ['Roboto', 'Arial'],
+		},
+		extend: {
+			colors: {
+				darkGray: '#0F0F0F',
+				lightGray: '#C1C1C1',
+			},
+		},
+	},
+	plugins: [],
 };

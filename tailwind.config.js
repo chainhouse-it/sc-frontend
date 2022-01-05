251c9e38c5b14175cff6aa74bb757ba76bba1e71
c0ndi <kowalik0konrad@gmail.com>
2022-01-05 10:12:16 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				darkGray: "#0F0F0F",
+				lightGray: "#C1C1C1",
+			},
+		},
+	},
+	plugins: [],
+};

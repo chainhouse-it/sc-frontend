c3856a810720b7d802ac966ccb32169a7659e7e7
c0ndi <kowalik0konrad@gmail.com>
2022-01-05 10:16:29 +0100

    Config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 module.exports = {
-	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
 	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
 		extend: {
 			colors: {
-				darkGray: "#0F0F0F",
-				lightGray: "#C1C1C1",
+				darkGray: '#0F0F0F',
+				lightGray: '#C1C1C1',
 			},
 		},
 	},

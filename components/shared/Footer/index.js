eaa5b18bcbed7c01aeb40d11e38ead5373aa7501
c0ndi <kowalik0konrad@gmail.com>
2022-01-08 01:32:51 +0100

    Footer mobile

--- components/shared/Footer/index.js
+++ components/shared/Footer/index.js
@@ -8,32 +8,30 @@ import {
 
 export default function Footer() {
 	return (
-		<div className='h-[350px] px-72 mt-72 py-12 text-white flex flex-col justify-between relative'>
+		<div className='h-[350px] lg:h-[32rem] px-48 mt-72 py-12 text-white flex flex-col justify-between relative xl:px-24 sm:px-12'>
 			<hr className='w-full' />
-			<p className='font-light text-4xl mb-auto py-10'>chainhouse.io</p>
-			<div className='flex justify-between w-full items-center'>
-				<div className='flex flex-row justify-around items-center text-md font-light'>
-					<p className='pr-8 cursor-pointer'>About</p>
-					<p className='pr-8 cursor-pointer'>
-						Work&nbsp;with&nbsp;us!
-					</p>
-					<p className='pr-8 cursor-pointer'>Contact</p>
-					<p className='pr-8 cursor-pointer'>Privacy&nbsp;policy</p>
+			<p className='text-4xl mb-auto py-10 font-bold'>chainhouse.io</p>
+			<div className='flex justify-between w-full items-center lg:flex-col lg:gap-10 lg:items-start'>
+				<div className='flex gap-6 flex-row items-center text-md font-light lg:flex-col lg:items-start'>
+					<p className='cursor-pointer'>About</p>
+					<p className='cursor-pointer'>Work&nbsp;with&nbsp;us!</p>
+					<p className='ursor-pointer'>Contact</p>
+					<p className='cursor-pointer'>Privacy&nbsp;policy</p>
 				</div>
-				<div className='flex flex-row justify-around items-center'>
-					<div className='cursor-pointer pl-8'>
+				<div className='flex flex-row items-center gap-6 lg:gap-0 lg:w-full lg:justify-between'>
+					<div className='cursor-pointer'>
 						<IoLogoGithub size='30' />
 					</div>
-					<div className='cursor-pointer pl-8'>
+					<div className='cursor-pointer'>
 						<IoMailSharp size='30' />
 					</div>
-					<div className='cursor-pointer pl-8'>
+					<div className='cursor-pointer'>
 						<IoLogoFacebook size='30' />
 					</div>
-					<div className='cursor-pointer pl-8'>
+					<div className='cursor-pointer'>
 						<IoLogoTwitter size='30' />
 					</div>
-					<div className='cursor-pointer pl-8'>
+					<div className='cursor-pointer'>
 						<IoLogoLinkedin size='30' />
 					</div>
 				</div>

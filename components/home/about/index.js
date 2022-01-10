fb9aaaea143c4ec752ec9a8f78846d89f5252d70
Piotrek <65945270+Piotrek-hub@users.noreply.github.com>
2022-01-10 20:48:29 +0100

    mobile about

--- components/home/about/index.js
+++ components/home/about/index.js
@@ -4,9 +4,9 @@ function AboutHero() {
 			<div className='relative flex'>
 				<img
 					src='/images/about-text.svg'
-					className='absolute bottom-3 z-10 2xl:scale-75 xl:top-1/2 xl:bottom-0 lg:hidden md:hidden'
+					className='absolute bottom-3 z-10 2xl:scale-75 xl:top-1/2 '
 				/>
-				<div className='h-8 border bg-[#0F0F0F] z-0 w-3/4 -translate-x-96 xl:hidden'></div>
+				<div className='h-8 border bg-[#0F0F0F] z-0 w-3/4 -translate-x-96 md:hidden xl:hidden'></div>
 			</div>
 		</div>
 	);
@@ -19,7 +19,7 @@ function TeamMember(props) {
 				<img className='w-full h-full absolute' src={props.imageUrl} />
 			</div>
 			<span className='text-[#9945FF] font-light'>{props.role}</span>
-			<span className='text-white text-2xl font-light'>
+			<span className='text-white text-2xl font-light md:w-48 md:text-center'>
 				{props.fullname}
 			</span>
 		</div>
@@ -30,9 +30,9 @@ export default function About() {
 	return (
 		<>
 			<AboutHero />
-			<div className='w-full flex flex-col justify-start '>
-				<div className='flex w-full px-48 py-32 justify-between items-center flex-row'>
-					<div className=' text-white block w-1/3 text-lg text-justify'>
+			<div className='w-full flex flex-col justify-start'>
+				<div className='flex w-full px-48 py-32 justify-around items-center flex-row md:py-16 md:px-5 md:flex-col '>
+					<div className=' text-white block w-1/2 text-lg text-justify md:text-xl md:w-full'>
 						View Player Transactions, their items and NFTs. Take
 						pride in your game collection and collect them. Vote for
 						video games and leave comments under players and
@@ -42,21 +42,22 @@ export default function About() {
 					</div>
 					<div className='w-1/2 h-max text-red-400'></div>
 				</div>
-				<div className='flex w-full flex-col px-48 items-start py-32'>
+				<div className='flex w-full flex-col px-48 items-start py-32 md:px-5 md:py-16'>
 					<span className='w-1/2 h-32 text-white block text-4xl font-light relative'>
 						Tech <span className='text-[#9945FF] '>stack</span>
 						<div className='absolute top-12 left-0 bg-[#ffffff] h-[1px] w-[150px]'></div>
 					</span>
-					<div className='w-max h-max '>
+					<div className='w-max h-max md:w-full'>
 						<img src='/images/tech-stack.png' />
 					</div>
 				</div>
-				<div className='flex w-full flex-col px-48 items-start py-32'>
-					<span className='w-1/2 h-32 text-white block text-4xl font-light relative'>
+				<div className='flex w-full flex-col px-48 items-start py-32 md:px-5 md:py-16'>
+					<span className='w-1/2 h-32 text-white block text-4xl font-light relative md:text-3xl '>
 						Meet our <span className='text-[#9945FF] '>team</span>
 						<div className='absolute top-12 left-0 bg-[#ffffff] h-[1px] w-[222px]'></div>
 					</span>
 					<div className='flex w-full flex-row h-max justify-evenly items-center flex-wrap lg:flex-col'>
+
 						<TeamMember
 							imageUrl='/images/binance.png'
 							role='Frontend dev'

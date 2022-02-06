f80ab875fa91ac8c456398850114921993feecef
c0ndi <73940688+c0ndi@users.noreply.github.com>
2022-02-06 22:04:39 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
 				/>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<main className=''>
+			<main>
 				{active ? (
 					<LoadingScreen />
 				) : (

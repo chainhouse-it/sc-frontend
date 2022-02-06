71307c07403376c24e6696781984b8b4bbe3a7a9
c0ndi <73940688+c0ndi@users.noreply.github.com>
2022-02-06 22:10:20 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import AboutWeb3 from '../components/Home/AboutWeb3';
+import AboutWeb3 from '../components/Home/aboutWeb3';
 import BlockchainIndustries from '../components/Home/blockchainIndustries';
 import ContactSection from '../components/Home/contactSection';
 import About from '../components/Home/howWeWork';

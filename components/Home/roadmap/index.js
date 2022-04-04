fc9cbd1cefaaa076ab326051233a2b3e4efff67e
Amelia Legan <53541434+deskop@users.noreply.github.com>
2022-04-04 12:58:53 +0200

    Update index.js

--- components/Home/roadmap/index.js
+++ components/Home/roadmap/index.js
@@ -20,22 +20,22 @@ export default function Roadmap() {
 			<div className='w-full text-[18px] flex justify-between leading-[50px] py-16 items-center text-center font-semibold lg:flex-col lg:w-full lg:gap-5 '>
 				<div className='w-1/5 border-b-2 bottom-border-gradient lg:w-full'>
 					<p className='bg-clip-text bg-gradient-to-br from-[#02FE85] to-[#02FED1] text-transparent cursor-pointer'>
-						Q4-2021
+						Q1-2022
 					</p>
 				</div>
 		<div className='w-full text-[18px] flex justify-between leading-[50px] py-16 items-center text-center font-semibold lg:flex-col lg:w-full lg:gap-5 '>
 				<div className='w-1/5 border-b-2 bottom-border-gradient lg:w-full'>
 					<p className='bg-clip-text bg-gradient-to-br from-[#02FE85] to-[#02FED1] text-transparent cursor-pointer'>
-						Q1-2022
-				</div>
-				<div className='w-1/5 border-b-2 border-[#62616F] lg:w-full'>
-					<p className='text-[#62616F] cursor-pointer'>Q2-2022</p>
+						Q2-2022
 				</div>
 				<div className='w-1/5 border-b-2 border-[#62616F] lg:w-full'>
 					<p className='text-[#62616F] cursor-pointer'>Q3-2022</p>
 				</div>
 				<div className='w-1/5 border-b-2 border-[#62616F] lg:w-full'>
 					<p className='text-[#62616F] cursor-pointer'>Q4-2022</p>
+				</div>
+				<div className='w-1/5 border-b-2 border-[#62616F] lg:w-full'>
+					<p className='text-[#62616F] cursor-pointer'>Q1-2023</p>
 				</div>
 			</div>
 
